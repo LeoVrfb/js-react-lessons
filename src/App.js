fad02800684c5fca87131de27b250f7522b69621
LeoVrfb <lnettoyeur75@gmail.com>
2023-05-03 19:52:04 +0200

    newmenu

--- src/App.js
+++ src/App.js
@@ -1,49 +1,80 @@
+import React from 'react'
 import './App.css';
-// import CallBack from './components/Js-lessons/CallBackFonctions/CallBack';
-// import Promises from './components/Js-lessons/CallBackFonctions/Promises';
-// import UseFull from './components/Js-lessons/CallBackFonctions/UseFull';
-// import Why from './components/Js-lessons/CallBackFonctions/Why';
-// import ControlerLeDom from './components/Js-lessons/ControlerLeDom';
-// import DixHacksJs from './components/Js-lessons/DixHacksJs';
-// import FonctionsEtTableaux from './components/Js-lessons/FonctionsEtTableaux';
-// import LesBoucles from './components/Js-lessons/LesBoucles';
-// import MethodesModify from './components/Js-lessons/lesMethodesEnJs/MethodesModify';
-// import MethodesNotModify from './components/Js-lessons/lesMethodesEnJs/MethodesNotModify';
-// import Tester from './components/Js-lessons/TestsPerso/Tester';
-import Chronometre from './components/React-Lessons/CycleDeVieComposants/Chronometre';
-import ChronometreFunc from './components/React-Lessons/CycleDeVieComposants/ChronometreFuncSansUseEffect';
-import CycleDeVie from './components/React-Lessons/CycleDeVieComposants/CycleDeVie';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthProvider from './components/React-Lessons/useContext/useContextLVL2/AuthProvider';
+import Menu from './components/Menu/Menu'
+import LesBoucles from './components/Js-lessons/LesBoucles'
+import FonctionsEtTableaux from './components/Js-lessons/FonctionsEtTableaux'
+import ControlerLeDom from './components/Js-lessons/ControlerLeDom'
+import DixHacksJs from './components/Js-lessons/DixHacksJs'
+import MethodesModify from './components/Js-lessons/lesMethodesEnJs/MethodesModify'
+import MethodesNotModify from './components/Js-lessons/lesMethodesEnJs/MethodesNotModify'
+import Why from './components/Js-lessons/CallBackFonctions/Why'
+import UseFull from './components/Js-lessons/CallBackFonctions/UseFull'
+import CallBack from './components/Js-lessons/CallBackFonctions/CallBack'
+import Promises from './components/Js-lessons/CallBackFonctions/Promises'
+import Tester from './components/Js-lessons/TestsPerso/Tester'
+import TesterChild from './components/Js-lessons/TestsPerso/TesterChild'
+import CycleDeVie from './components/React-Lessons/CycleDeVieComposants/CycleDeVie'
+import Chronometre from './components/React-Lessons/CycleDeVieComposants/Chronometre'
+import ChronometreFunc from './components/React-Lessons/CycleDeVieComposants/ChronometreFuncSansUseEffect'
+import ChronometreFuncWithUseEffect from './components/React-Lessons/CycleDeVieComposants/ChronometreFuncWithUseEffect'
+import ChronometreFuncUse from './components/React-Lessons/CycleDeVieComposants/ChronometreFuncWithUseEffect';
+import RefClasse from './components/React-Lessons/References/RefClasse';
 import UseRef from './components/React-Lessons/References/UseRef';
+import UseContextLVL1 from './components/React-Lessons/useContext/useContextLVL1/UseContextLVL1';
+import UseContextLVL2 from './components/React-Lessons/useContext/useContextLVL2/UseContextLVL2';
+import UseReducerLVL1 from './components/React-Lessons/useReducer/useReducerLVL1';
+import UseReducerLVL2 from './components/React-Lessons/useReducer/useReducerLVL2';
 import UseReducerLVL3 from './components/React-Lessons/useReducer/ComplexUseReducer/useReducerLVL3';
-import CounterWithReducer from './components/React-Lessons/useReducer/useReducerLVL1';
-// import TesterChild from './components/Js-lessons/TesterChild';
-// import Tictactoe from './components/Tictactoe/Tictactoe';
-// import CompteurJours from './components/appcompteurJours/CompteurJours';
+import Tictactoe from './components/React-Lessons/Tictactoe/Tictactoe';
+
+
+
+
 
 
 function App() {
   return (
     <div className="App">
+      
 
-      {/* <Tictactoe />  */}
-      {/* <CompteurJours />
-      <MethodesModify />
-      < MethodesNotModify /> */}
-      {/* <FonctionsEtTableaux /> */}
-      {/* <LesBoucles /> */}
-      {/* <ControlerLeDom /> */}
-      {/* < DixHacksJs /> */}
-      {/* <Why /> */}
-      {/* <CallBack /> */}
-      {/* <UseFull /> */}
-      {/* < Tester /> */}
-      {/* < Promises /> */}
-      <UseRef />
-      <CycleDeVie />
-      <Chronometre />
-      <ChronometreFunc />
-      <CounterWithReducer />
-      <UseReducerLVL3 />
+      <AuthProvider>
+
+        <Router>
+        <Menu />
+          <Routes>
+            <Route path='/notionsdejs/lesbouclesjs' element={<LesBoucles />} />
+            <Route path='/notionsdejs/fonctionsettableaux' element={<FonctionsEtTableaux />} />
+            <Route path='/notionsdejs/controlerledom' element={<ControlerLeDom />} />
+            <Route path='/notionsdejs/dixhacks' element={<DixHacksJs />} />
+            <Route path='/notionsdejs/methodesjsmutatrices' element={<MethodesModify />} />
+            <Route path='/notionsdejs/methodesjsnonmutatrices' element={<MethodesNotModify />} />
+            <Route path='/notionsdejs/why' element={<Why />} />
+            <Route path='/notionsdejs/usefull' element={<UseFull />} />
+            <Route path='/notionsdejs/callback' element={<CallBack />} />
+            <Route path='/notionsdejs/promises' element={<Promises />} />
+            <Route path='/notionsdejs/tester' element={<Tester />} />
+            <Route path='/notionsdejs/testerchild' element={<TesterChild />} />
+            <Route path='/react/cycledevie' element={<CycleDeVie />} />
+            <Route path='/react/chronoclasse' element={<Chronometre />} />
+            <Route path='/react/chronofuncnouseff' element={<ChronometreFunc />} />
+            <Route path='/react/chronofuncuseff' element={<ChronometreFuncUse />} />
+            <Route path='/react/chronofuncuseff' element={<ChronometreFuncWithUseEffect />} />
+            <Route path='/react/refclasse' element={<RefClasse />} />
+            <Route path='/react/useref' element={<UseRef />} />
+            <Route path='/react/usecontextlvl1' element={<UseContextLVL1 />} />
+            <Route path='/react/usecontextlvl2' element={<UseContextLVL2 />} />
+            <Route path='/react/usereducerlvl1' element={<UseReducerLVL1 />} />
+            <Route path='/react/usereducerlvl2' element={<UseReducerLVL2 />} />
+            <Route path='/react/usereducerlvl3' element={<UseReducerLVL3 />} />
+            <Route path='/react/tictactoe' element={<Tictactoe />} />
+          
+          
+          </Routes>
+        </Router>
+      </AuthProvider>
+
     </div>
   );
 }
